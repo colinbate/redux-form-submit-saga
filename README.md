b6d1806988bf9e30220a4429313168c1d078fa44
Colin Bate <colin@colinbate.com>
2016-07-08 01:30:50 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
 # redux-form-submit-saga
-Handled redux-form submissions using redux-saga
+
+Handles redux-form submissions using redux-saga
+
+## Install
+
+```
+npm install -S redux-form-submit-saga
+```
+
+## Usage
+
+### Integrate with Sagas
+
+You can integrate this package with your other sagas as follows:
+
+```js
+import {createSagaMiddleware} from 'redux-saga';
+import {addFormSubmitSagaTo} from 'redux-form-submit-saga';
+import {allMySagas} from '...';
+
+const sagaMiddleware = createSagaMiddleware();
+// ... create store with middleware ...
+const rootSaga = addFormSubmitSagaTo(allMySagas);
+sagaMiddleware.run(rootSaga);
+```
+
+If you need to access the saga directly, it can be imported as `formSubmitSaga`.
+
+### Integrate with Forms
+
+For each form where you want to submit with something async (often), you can setup the submit function like this:
+
+```js
+import {reduxForm, ...} from 'redux-form';
+import {onSubmitActions} from 'redux-form-submit-saga';
+
+// ... define MyForm ...
+
+export default reduxForm({
+  form: 'myForm',
+  onSubmit: onSubmitActions('MY_FORM')
+})(MyForm);
+```
+
+This is assuming you are using `handleSubmit` within your form and not passing it anything.
+
+There are multiple ways to configure `onSubmitActions`, the simplest being the one above.
+
+#### `onSubmitActions(root: string, transform?: Function)`
+
+You pass in a string, and the action types are derived from that with the suffixes: SUBMIT, SUCCESS, and FAILURE. You can optionally provide a transform function which will be used to transform the form values before being added to the submit action.
+
+#### `onSubmitActions(submit: string | Function, success: string, failure: string)`
+
+Here you can pass each action type separately. The submit action (first parameter) can be an action creator function if desired.
+
+### Handle Submissions using Sagas
+
+Once set up you should be able to do the following:
+
+```js
+import * as api from '...';
+import {call, put} from 'redux-saga/effects';
+import {takeLatest} from 'redux-saga';
+
+function* myFormSaga (action) {
+  try {
+    const response = yield call(api.submitMyForm, action.payload);
+    yield put({type: 'MY_FORM_SUCCESS', payload: response.body});
+  } catch (err) {
+    yield put({type: 'MY_FORM_FAILURE', payload: {_error: err.message}});
+  }
+}
+
+export function* watchMyForm () {
+  yield* takeLatest('MY_FORM_SUBMIT', myFormSaga);
+}
+```
---
+++ index.js
@@ -0,0 +1,96 @@
+import {put, take, race, call, fork} from 'redux-saga/effects';
+import {SubmissionError} from 'redux-form';
+
+const identity = f => f;
+const FORM_SUBMIT = 'redux-form-submit-saga/FORM_SUBMIT';
+export const SUFFIX = [
+  'SUBMIT',
+  'SUCCESS',
+  'FAILURE'
+];
+
+function formSubmit (submitCreator, successActionType, failureActionType, values, resolve, reject) {
+  return {
+    type: FORM_SUBMIT,
+    meta: {
+      successActionType,
+      failureActionType,
+      resolve,
+      reject
+    },
+    payload: submitCreator(values)
+  };
+}
+
+const creatorFactory = (type, transform) => payload => ({
+  type,
+  payload: transform(payload)
+});
+
+function normalizeShortSig (root, transform = identity) {
+  const types = SUFFIX.map(s => `${root}_${s}`);
+  types[0] = creatorFactory(types[0], transform);
+  return types;
+}
+
+function normalizeLongSig (submit, success, failure) {
+  const submitCreator = typeof submit === 'string' ? creatorFactory(submit, identity) : submit;
+  return [submitCreator, success, failure];
+}
+
+function normalizeActionArray (input) {
+  if (!input.length) {
+    throw new Error('You must provide action types to onSubmitActions');
+  }
+  if (typeof input[0] === 'object') {
+    return normalizeLongSig(input[0].submit, input[0].success, input[0].failure);
+  }
+  if (input.length > 2) { // eslint-disable-line no-magic-numbers
+    return normalizeLongSig(...input);
+  }
+  return normalizeShortSig(...input);
+}
+
+export function onSubmitActions (...actions) {
+  const actionArray = normalizeActionArray(actions);
+  return (values, dispatch) =>
+    new Promise((resolve, reject) => {
+      dispatch(formSubmit(...actionArray, values, resolve, reject));
+    });
+}
+
+export function* formSubmitSaga () {
+  while (true) { // eslint-disable-line no-constant-condition
+    const {
+      meta: {
+        successAction,
+        failureAction,
+        resolve,
+        reject
+      },
+      payload
+    } = yield take(FORM_SUBMIT);
+    yield put(payload);
+
+    const {success, failure} = yield race({
+      success: take(successAction),
+      failure: take(failureAction)
+    });
+
+    if (success) {
+      yield call(resolve, success.payload);
+    } else {
+      yield call(reject, new SubmissionError(failure.payload));
+    }
+  }
+}
+
+export function addFormSubmitSagaTo (others) {
+  if (!others) {
+    return fork(formSubmitSaga);
+  }
+  if (Array.isArray(others)) {
+    return [...others, fork(formSubmitSaga)];
+  }
+  return [others, fork(formSubmitSaga)];
+}
